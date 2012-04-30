b8519f99a33605c666adaba47fa444d276238f62
robotconscience <brett@robotconscience.com>
2012-04-30 13:28:10 -0400

    moved all non-standard addons to openTSPS/addons! (need to update win and linux files)

--- standalone/multicamera/addons/ofxKinect/src/ofxBase3DVideo.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-class ofxBase3DVideo: public ofBaseVideo {
-public:
-	virtual unsigned char * getDepthPixels()=0; // grey scale values
-	virtual float * getDistancePixels()=0;
-	virtual ofTexture & getDepthTextureReference()=0;
-};
--- standalone/multicamera/addons/ofxTSPS/src/utils/ofxTSPSUtils.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  ofxTSPSUtils.cpp
-//  openTSPS
-//
-//  Created by brenfer on 11/11/11.
-//  Copyright 2011 Rockwell Group. All rights reserved.
-//
-
-#include "ofxTSPSUtils.h"
