c058c06adf23e098819169e1e991f91a18b3a3d5
Brett Renfer <brett@robotconscience.com>
2013-01-23 11:05:02 -0500

    OpenNI example fixed

--- standalone/openNI/src/OpenNI.h
+++ standalone/openNI/src/OpenNI.h
@@ -15,12 +15,17 @@ namespace ofxTSPS {
     class OpenNI : public Source, public ofxOpenNI {
     public:
         
-        OpenNI() : Source(){
+        OpenNI() : Source(), ofxOpenNI()
+        {
             // type defaults to CAMERA_CUSTOM
             bCanTrackHaar = false;
-            invertedPixels.allocate( 640,480, OF_IMAGE_GRAYSCALE);
+            invertedPixels.allocate( 320,240, OF_IMAGE_GRAYSCALE);
+            tiltAmount = 0;
         }
-                
+        
+        /** Tilt amount (-1 to 1)  */
+        float tiltAmount;
+        
         // core
         bool available(){
             return (getNumDevices() >= 1);
@@ -33,8 +38,23 @@ namespace ofxTSPS {
                 ofPixelsRef pix = getDepthPixels();
                 int channels    = pix.getNumChannels();
                 int dims        = invertedPixels.getWidth() * invertedPixels.getHeight();
+                float shift       = 0.0f;
+                
                 for ( int i=0; i<dims; i++){
-                    invertedPixels[i] = pix[2+i * channels] == 0 ? 0 : abs(255-pix[2+i * channels]);
+                    int y = i / getWidth();
+                    
+                    if ( tiltAmount > 0 ){
+                        float div = (float) y/getHeight();
+                        shift = (div * tiltAmount);
+                    } else if ( tiltAmount < 0) {
+                        float div = ((float) (getHeight()-y)/getHeight() );
+                        shift = div * (-tiltAmount);
+                    }
+                    // invert
+                    invertedPixels[i] = pix[2+i * channels] == 0 ? 0 : fabs(255.0f-pix[2+i * channels]);
+                    
+                    // shift
+                    invertedPixels[i] *= (float) (1.0f-shift);
                 }
             }
         }
@@ -54,13 +74,17 @@ namespace ofxTSPS {
         
         bool openSource( int width, int height, string etc="" ){
             bool bSetup = isContextReady();
-            if(!bSetup) bSetup = setup();
-            if ( bSetup ){
+            if(!bSetup) bIsOpen = setup(true, width, height);
+            if ( bIsOpen ){
+                if ( invertedPixels.getWidth() != width || invertedPixels.getHeight() != height ){
+                    invertedPixels.clear();
+                    invertedPixels.allocate( width, height, OF_IMAGE_GRAYSCALE);
+                }
                 setDepthColoring(COLORING_BLUES);
                 addDepthGenerator();
                 start();                
             }
-            return bSetup;
+            return bIsOpen;
         }
         
         void closeSource(){
