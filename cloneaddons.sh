e098326e3d482829414ed35b040f0ab30fa83f73
Brett Renfer <brett@robotconscience.com>
2015-04-30 13:17:11 -0400

    Updated submods + add cloneaddons

---
+++ cloneaddons.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#make sure you are in the TSPS folder when you run this.
+
+cd ../addons
+
+
+PREFIX="git clone https://github.com/"
+
+# source
+${PREFIX}elliotwoods/ofxKinectForWindows2 --branch 0.8.4
